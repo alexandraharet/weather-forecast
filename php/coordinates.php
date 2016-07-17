218c9b06ef3f891e052021f5a65d6d724764496a
Alexandra Haret <alexandra.haret@gmail.com>
2016-07-17 20:55:35 +0100

    Changed intput type to text box to allow any address to be entered; store weather icon images locally; started working on styling

--- php/coordinates.php
+++ php/coordinates.php
@@ -1,18 +1,17 @@
 <?php
-
 // api at https://developers.google.com/maps/documentation/geocoding/start
 // max 2,500 requests per day, 50 requests per second
-// server key for Google geolocation API: 'AIzaSyBYlvKDvzGZU2X7SGE6vPUZcoWwPOlavec'
+// object example CITY ONLY: https://maps.googleapis.com/maps/api/geocode/json?address=Bucharest&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY
+// object exaple FULL ADDRESS: https://maps.googleapis.com/maps/api/geocode/json?address=Siret+6%2C+Barlad%2C+Romania&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY
 
 	$api_maps = 'https://maps.googleapis.com/maps/';
-	$city_name = $_GET['cityName'];
+	$address = urlencode($_GET['address']);
 	$api_key1 = 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY';
-	$url = $api_maps . 'api/geocode/json?address=' . $city_name . '&key=' . $api_key1;
+	$url = $api_maps . 'api/geocode/json?address=' . $address . '&key=' . $api_key1;
 	$curl = curl_init($url);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 	$curl_response = curl_exec($curl);
 	curl_close($curl);
   echo ($curl_response);
-
 
 ?>
