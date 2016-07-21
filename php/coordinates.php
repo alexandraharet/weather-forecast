f1a7eeef96aead970a291c66389ece1665a016f8
Alexandra Haret <alexandra.haret@gmail.com>
2016-07-21 10:07:32 +0100

    added three data points for currently, tonight and tomorrow, updated styling and CSS using bootstrap

--- php/coordinates.php
+++ php/coordinates.php
@@ -4,14 +4,14 @@
 // object example CITY ONLY: https://maps.googleapis.com/maps/api/geocode/json?address=Bucharest&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY
 // object exaple FULL ADDRESS: https://maps.googleapis.com/maps/api/geocode/json?address=Siret+6%2C+Barlad%2C+Romania&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY
 
-	$api_maps = 'https://maps.googleapis.com/maps/';
-	$address = urlencode($_GET['address']);
-	$api_key1 = 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY';
-	$url = $api_maps . 'api/geocode/json?address=' . $address . '&key=' . $api_key1;
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	$curl_response = curl_exec($curl);
-	curl_close($curl);
-  echo ($curl_response);
+$api_maps = 'https://maps.googleapis.com/maps/';
+$address = urlencode($_GET['address']);
+$api_key1 = 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY';
+$url = $api_maps . 'api/geocode/json?address=' . $address . '&key=' . $api_key1;
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$curl_response = curl_exec($curl);
+curl_close($curl);
+echo ($curl_response);
 
 ?>
