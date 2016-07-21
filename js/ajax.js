034a9d418a1016026a66978b2fe8e8a1f47502be
Alexandra Haret <Alexandra Haret>
2016-07-21 10:07:32 +0100

    added three data points for currently, tonight and tomorrow, updated styling and CSS using bootstrap

--- js/ajax.js
+++ js/ajax.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
       dataType: 'json',
       success: function(data) {
         getLatLng(data);
-    },
+      },
       error: function(xhr, desc, err) {
         console.log(xhr);
         console.log("Details: " + desc + "\nError:" + err);
@@ -29,49 +29,70 @@ $(document).ready(function(){
       url: 'php/forecast.php',
       type: 'GET',
       data: {lat: latitude,
-             lon: longitude},
-      dataType: 'json',
-      success: function(data) {
-        getDivIdsFromHtml('currently');
-        parseAndRenderWeather('currently', data);
-      },
-      error: function(xhr, desc, err) {
-        console.log(xhr);
-        console.log("Details: " + desc + "\nError:" + err);
-      }
-    });
-}
-  // gets div ids from HMTL, to determine which data must be retrieved from the two APIs used
-  var getDivIdsFromHtml = function (myClass) {
-    classDivId = [];
-    $('.'+ myClass).children('div').each(function() {
-      classDivId.push($(this).attr('id'));
-    });
-    return classDivId;
-  }
+        lon: longitude},
+        dataType: 'json',
+        success: function(data) {
+          getDivIdsFromHtml('currently');
+          parseAndRenderWeatherCurrently('currently', data);
+          getDivIdsFromHtml('tonight');
+          parseAndRenderWeather('tonight', data);
+          getDivIdsFromHtml('tomorrow');
+          parseAndRenderWeather('tomorrow', data);
+        },
+        error: function(xhr, desc, err) {
+          console.log(xhr);
+          console.log("Details: " + desc + "\nError:" + err);
+        }
+      });
+    }
+    // gets div ids from HMTL, to determine which data must be retrieved from the two APIs used
+    var getDivIdsFromHtml = function (myClass) {
+      classDivId = [];
+      $('.'+ myClass).children('div').each(function() {
+        classDivId.push($(this).attr('id'));
+      });
+      return classDivId;
+    }
 
-  // renders the location information for the address entered
-  var parseAndRenderLocation = function (myClass, data) {
-    var divs = getDivIdsFromHtml(myClass);
-    $.each(divs, function (key, value) {
+    // renders the location information for the address entered
+    var parseAndRenderLocation = function (myClass, data) {
+      var divs = getDivIdsFromHtml(myClass);
+      $.each(divs, function (key, value) {
         $("#" + value).html("Location: " + data.results[0].formatted_address);
-    })
-  }
+      })
+    }
 
-  // renders the weather data for the address entered
-  var parseAndRenderWeather = function (myClass, data) {
-    var divs = getDivIdsFromHtml(myClass);
-    $.each(divs, function (key, value) {
-      if (value == "icon") {
-        var weatherIs = data.currently.icon;
-        $("#icon").html('<img src="img/' + weatherIs + '.png" alt="' + weatherIs + '" width="120" height="120" />');
-      }
+    // renders the weather data for the address entered
+    var parseAndRenderWeatherCurrently = function (myClass, data) {
+      var divs = getDivIdsFromHtml(myClass);
+      $.each(divs, function (key, value) {
+        if (value == "icon") {
+          $("." + myClass + " #icon").html('<img src="img/' + data.currently.icon + '.png" alt="' + data.currently.icon + '"  />');
+        }
+        else if (value == "temperature") {
+          temperature = ((data.currently[value]) - 32) * 5 / 9;
+          $("." + myClass + " #" + value).html(value + ": " + temperature.toFixed(0) + " °C");
+        }
+        else $("." + myClass + " #" + value).html(value + ": " + data.currently[value]);
+      })
+    }
 
-      else if (value == "temperature") {
-        temperature = ((data.currently[value]) - 32) * 5 / 9;
-        $("#" + value).html(value + ": " + temperature.toFixed(0) + " °C");
-      }
-      else $("#" + value).html(value + ": " + data.currently[value]);
-    })
-  }
-});
+
+    var parseAndRenderWeather = function (myClass, data) {
+      var divs = getDivIdsFromHtml(myClass);
+      var d = new Date(data.currently.time * 1000);
+      if (myClass == "tonight") dataIndex = 24 - d.getHours() + data.offset;
+      else if (myClass == "tomorrow") dataIndex = 36 - d.getHours() + data.offset;
+      $.each(divs, function (key, value) {
+        if (value == "icon") {
+          $("." + myClass + " #icon").html('<img src="img/' + data.hourly.data[dataIndex].icon + '.png" alt="' + data.hourly.data[dataIndex].icon + '"  />');
+        }
+        else if (value == "temperature") {
+          temperature = ((data.hourly.data[dataIndex][value]) - 32) * 5 / 9;
+          $("." + myClass + " #" + value).html(value + ": " + temperature.toFixed(0) + " °C");
+        }
+        else $("." + myClass + " #" + value).html(value + ": " + data.hourly.data[dataIndex][value]);
+      })
+    }
+
+  });
--- php/forecast.php
+++ php/forecast.php
@@ -2,17 +2,17 @@
 // api at https://developer.forecast.io/docs/v2
 // max 1000 calls per day
 // object example: https://api.forecast.io/forecast/2cb2a6600011ce4ca629efa9e07cc9bd/55.9533,3.1883
-// object example with EXCLUDE: https://api.forecast.io/forecast/2cb2a6600011ce4ca629efa9e07cc9bd/55.9533,3.1883?exclude=minutely,hourly,flags
+// object example with EXCLUDE: https://api.forecast.io/forecast/2cb2a6600011ce4ca629efa9e07cc9bd/55.9533,3.1883?exclude=minutely,daily,flags
 
-	$api_weather = 'https://api.forecast.io/forecast/';
-	$api_key2 = '2cb2a6600011ce4ca629efa9e07cc9bd';
-	$latitude = $_GET['lat'];
-	$longitude = $_GET['lon'];
-	$url = $api_weather . $api_key2 . '/' . $latitude . ',' . $longitude . '?exclude=minutely,daily,flags';
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	$curl_response = curl_exec($curl);
-	curl_close($curl);
-  echo ($curl_response);
+$api_weather = 'https://api.forecast.io/forecast/';
+$api_key2 = '2cb2a6600011ce4ca629efa9e07cc9bd';
+$latitude = $_GET['lat'];
+$longitude = $_GET['lon'];
+$url = $api_weather . $api_key2 . '/' . $latitude . ',' . $longitude . '?exclude=minutely,daily,flags';
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$curl_response = curl_exec($curl);
+curl_close($curl);
+echo ($curl_response);
 
 ?>
