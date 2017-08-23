cb3458171312fba782d81d4f3d7186a47ec259bb
Alexandra Haret <alexandra.haret@gmail.com>
2017-08-23 10:30:52 +0100

    removed google api key

--- php/coordinates.php
+++ php/coordinates.php
@@ -1,12 +1,12 @@
 <?php
 // api at https://developers.google.com/maps/documentation/geocoding/start
 // max 2,500 requests per day, 50 requests per second
-// object example CITY ONLY: https://maps.googleapis.com/maps/api/geocode/json?address=Bucharest&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY
-// object exaple FULL ADDRESS: https://maps.googleapis.com/maps/api/geocode/json?address=Siret+6%2C+Barlad%2C+Romania&key=AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY
+// object example CITY ONLY: https://maps.googleapis.com/maps/api/geocode/json?address=Bucharest&key=YOUR_KEY_HERE
+// object exaple FULL ADDRESS: https://maps.googleapis.com/maps/api/geocode/json?address=Siret+6%2C+Barlad%2C+Romania&key=YOUR_KEY_HERE
 
 $api_maps = 'https://maps.googleapis.com/maps/';
 $address = urlencode($_GET['address']);
-$api_key1 = 'AIzaSyBvzrK2_Fr84Od185gbDGxlpRapNcJE4BY';
+$api_key1 = 'YOUR_KEY_HERE';
 $url = $api_maps . 'api/geocode/json?address=' . $address . '&key=' . $api_key1;
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
